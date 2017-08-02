4ae961fe5d4358b9c98f4bcd8f4ddd38fce40bae
huanhuan <huanfuan@163.com>
2017-08-02 21:15:20 +0800

    增加接口main.py

--- main.py
+++ main.py
@@ -7,19 +7,31 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+from handler.IndexHandler import IndexHandler
+from handler.HomeHandler import HomeHandler
+from handler.WorkHandler import WorkHandler
+from handler.AboutHandler import AboutHandler
+from handler.ContactHandler import ContactHandler
+
 
 from tornado.options import define, options
 define("port", default=8083, help="run on the given port", type=int)
 
-class IndexHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('index.html')
+# class IndexHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.render('index.html')
 
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     app = tornado.web.Application(
-        handlers=[(r'/', IndexHandler)],
+        handlers=[
+            (r'/', IndexHandler),
+            (r'/about',AboutHandler),
+            (r'/home',HomeHandler),
+            (r'work',WorkHandler),
+            (r'contact',ContactHandler),
+        ],
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         debug=True
